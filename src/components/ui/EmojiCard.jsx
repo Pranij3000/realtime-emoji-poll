2aab3efd55055a5868e151bea2e8e57e74ed80fc
Pranij Manandhar <pranij.makura@gmail.com>
2025-09-15 12:54:35 +0545

    minor changes

--- src/components/ui/EmojiCard.jsx
+++ src/components/ui/EmojiCard.jsx
@@ -3,7 +3,7 @@ export default function EmojiCard({ icon, votes }) {
     <>
       <button className="mx-auto flex gap-2 items-center justify-center cursor-pointer border-1 border-gray-400 rounded-2xl w-fit py-4 pl-2 pr-4 bg-white dark:bg-gray-700">
         <span className="icon text-6xl">{icon}</span>
-        <strong className="text-4xl">{votes}</strong>
+        <strong className="text-4xl text-title dark:text-dark-title">{votes}</strong>
       </button>
     </>
   );
