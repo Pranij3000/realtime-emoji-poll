cc63f35fbcf302842a8bcda576fe8bcdb2a6ff9a
Pranij Manandhar <pranij.makura@gmail.com>
2025-09-17 09:11:28 +0545

    minor changes

--- src/components/ui/EmojiCard.jsx
+++ src/components/ui/EmojiCard.jsx
@@ -1,7 +1,11 @@
+import { useState } from "react";
+
 export default function EmojiCard({ icon, votes }) {
+  const [selected, setSelected] = useState(false);
+
   return (
     <>
-      <button className="mx-auto flex gap-2 items-center justify-center cursor-pointer border-1 border-gray-400 rounded-2xl w-fit py-4 pl-2 pr-4 bg-white dark:bg-gray-700">
+      <button onClick={() => setSelected(!selected)} className={`${selected ? "bg-blue-400" : "bg-white dark:bg-gray-700"} mx-auto flex gap-2 items-center justify-center cursor-pointer border-1 border-gray-400 rounded-2xl w-fit py-4 pl-2 pr-4`}>
         <span className="icon text-6xl">{icon}</span>
         <strong className="text-4xl text-title dark:text-dark-title">{votes}</strong>
       </button>
