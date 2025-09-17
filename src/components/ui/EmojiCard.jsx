5ff59215b59e73cbadc252a0d4ef449d7384a934
Pranij Manandhar <pranij.makura@gmail.com>
2025-09-17 11:35:00 +0545

    selected fixed

--- src/components/ui/EmojiCard.jsx
+++ src/components/ui/EmojiCard.jsx
@@ -1,14 +1,12 @@
 import { useState } from "react";
 
-export default function EmojiCard({ icon, votes }) {
-  const [selected, setSelected] = useState(false);
-
-  return (
-    <>
-      <button onClick={() => setSelected(!selected)} className={`${selected ? "bg-blue-400" : "bg-white dark:bg-gray-700"} mx-auto flex gap-2 items-center justify-center cursor-pointer border-1 border-gray-400 rounded-2xl w-fit py-4 pl-2 pr-4`}>
-        <span className="icon text-6xl">{icon}</span>
-        <strong className="text-4xl text-title dark:text-dark-title">{votes}</strong>
-      </button>
-    </>
-  );
+export default function EmojiCard({ icon, votes, setSelected }) {
+	return (
+		<>
+			<button onClick={() => setSelected(icon)} className={` "bg-blue-400" "bg-white dark:bg-gray-700" mx-auto flex gap-2 items-center justify-center cursor-pointer border-1 border-gray-400 rounded-2xl w-fit py-4 pl-2 pr-4`}>
+				<span className="icon text-6xl">{icon}</span>
+				<strong className="text-4xl text-title dark:text-dark-title">{votes}</strong>
+			</button>
+		</>
+	);
 }
