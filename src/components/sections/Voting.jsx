8c3bf90a297bbbda07a69ae82689707851285cc2
Pranij Manandhar <pranij.makura@gmail.com>
2025-09-18 22:00:17 +0545

    click animation added

--- src/components/sections/Voting.jsx
+++ src/components/sections/Voting.jsx
@@ -2,24 +2,24 @@ import { useState } from "react";
 import EmojiCard from "../ui/EmojiCard";
 
 export default function Voting() {
-	const [selected, setSelected] = useState(false);
-	const emoji = [
-		{ icon: "🎉", votes: 4 },
-		{ icon: "😂", votes: 4 },
-		{ icon: "😍", votes: 4 },
-		{ icon: "🚀", votes: 4 },
-		{ icon: "🌈", votes: 4 },
-		{ icon: "🔥", votes: 4 },
-	];
-	return (
-		<section className="voting bg-background dark:bg-dark-background">
-			<div className="container mx-auto px-3 py-5">
-				<div className="grid grid-cols-2 md:grid-cols-3 gap-6 max-w-[500px] mx-auto">
-					{emoji.map((item, index) => (
-						<EmojiCard key={index} icon={item.icon} votes={item.votes} setSelected={setSelected} selected={selected} />
-					))}
-				</div>
-			</div>
-		</section>
-	);
+  const [selected, setSelected] = useState(null);
+  const emoji = [
+    { icon: "🎉", votes: 4 },
+    { icon: "😂", votes: 4 },
+    { icon: "😍", votes: 4 },
+    { icon: "🚀", votes: 4 },
+    { icon: "🌈", votes: 4 },
+    { icon: "🔥", votes: 4 },
+  ];
+  return (
+    <section className="voting bg-background dark:bg-dark-background">
+      <div className="container mx-auto px-3 py-5">
+        <div className="grid grid-cols-2 md:grid-cols-3 gap-6 max-w-[500px] mx-auto">
+          {emoji.map((item, index) => (
+            <EmojiCard key={index} icon={item.icon} votes={item.votes} setSelected={setSelected} isSelected={selected === item.icon} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 }
--- src/components/ui/EmojiCard.jsx
+++ src/components/ui/EmojiCard.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
 
-export default function EmojiCard({ icon, votes, setSelected }) {
-	return (
-		<>
-			<button onClick={() => setSelected(icon)} className={` "bg-blue-400" "bg-white dark:bg-gray-700" mx-auto flex gap-2 items-center justify-center cursor-pointer border-1 border-gray-400 rounded-2xl w-fit py-4 pl-2 pr-4`}>
-				<span className="icon text-6xl">{icon}</span>
-				<strong className="text-4xl text-title dark:text-dark-title">{votes}</strong>
-			</button>
-		</>
-	);
+export default function EmojiCard({ icon, votes, setSelected, isSelected }) {
+  return (
+    <>
+      <button onClick={() => setSelected(icon)} className={`${isSelected ? "scale-120 shadow-xl bg-blue-100 dark:bg-gray-700" : ""} transition-all mx-auto flex gap-2 items-center justify-center cursor-pointer border-1 border-gray-400 rounded-2xl w-fit py-4 pl-2 pr-4`}>
+        <span className="icon text-6xl">{icon}</span>
+        <strong className="text-4xl text-title dark:text-dark-title">{votes}</strong>
+      </button>
+    </>
+  );
 }
